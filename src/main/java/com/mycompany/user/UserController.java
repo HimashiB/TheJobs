ce73097795e6b89670facd2e3d87d2e18c29e537
Himashi Bandara <himashi08@gmail.com>
2023-09-03 02:59:41 +0530

    backend created for reg and login

---
+++ src/main/java/com/mycompany/user/UserController.java
@@ -0,0 +1,28 @@
+package com.mycompany.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        // create model object to store form data
+        User user = new User();
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String saveUser(User user){
+        service.save(user);
+
+        return "redirect:/login";
+    }
+}
