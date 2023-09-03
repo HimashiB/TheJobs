639a8cad3433cf88f6b3a135ce21cff5750895e7
Himashi Bandara <himashi08@gmail.com>
2023-09-03 09:55:37 +0530

    server side updated

---
+++ src/main/java/com/mycompany/MainController.java
@@ -0,0 +1,29 @@
+package com.mycompany;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/index")
+    public String showHomePage() {
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String showAboutPage() {
+        return "about";
+    }
+
+    @GetMapping("/jobs")
+    public String showJobsPage() {
+        return "jobs";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+
+}
---
+++ src/main/java/com/mycompany/user/UserService.java
@@ -0,0 +1,15 @@
+package com.mycompany.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public void save(User user) {
+        repo.save(user);
+    }
+}
