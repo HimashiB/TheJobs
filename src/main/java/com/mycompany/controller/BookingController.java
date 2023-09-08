556756f328214122c43e07f831aa93f0601f415f
Himashi Bandara <himashi08@gmail.com>
2023-09-09 00:53:06 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/mycompany/controller/BookingController.java
+++ src/main/java/com/mycompany/controller/BookingController.java
@@ -10,9 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class BookingController {
@@ -25,6 +27,7 @@ public class BookingController {
 
     @Autowired
     JobTypesRepository jobrepo;
+
 
     @GetMapping("/appointment")
     public String showAppointmentPage(Model model){
@@ -40,15 +43,27 @@ public class BookingController {
 
     @GetMapping("/history")
     public String appointmentHistory(Model model){
-        List<Booking> listAppointments = repo.findAll();
+        List<Booking> listAppointments = (List<Booking>) repo.findAll();
         model.addAttribute("listAppointments", listAppointments);
         return "history";
     }
 
-    @PostMapping("/bookings/save")
+    @PostMapping("/appointment/save")
     public String saveBooking(Booking booking){
         repo.save(booking);
         return "redirect:/history";
     }
 
+    @GetMapping("history/edit/{id}")
+    public String showEditAppForm(@PathVariable("id") Integer id, Model model){
+        System.out.println("This is the id" + id);
+        Optional<Booking> bookingOptional = repo.findById(id);
+        model.addAttribute("booking", bookingOptional.get());
+        System.out.println("Booking Country" + bookingOptional);
+        model.addAttribute("listCountries", countryrepo.findAll());
+        model.addAttribute("listJobs", jobrepo.findAll());
+        /*LocalDate bookingDate = bookingOptional.get().getDate();
+        model.addAttribute("date", bookingDate);*/
+        return "appointment";
+    }
 }
